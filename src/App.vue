bf3662db44fb4b1877dd4dba66e4f8add8acf290
Kevin Ngo <ngokevin39@gmail.com>
2019-11-03 20:08:22 -0500

    implementing popover, remove API request and store all data in a JSON

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,113 @@
 <template>
   <div id="app">
     <Header />
+
     <div class="flex-container">
-      <div v-for="pokemon in pokemons" :key="pokemon.id" id="card" @click='showDetails($event)'>
+      <div v-for="pokemon in pokemons" :key="pokemon.id" id="card" @click="openPopover(pokemon, pokemon.id)">
         <cName :pokeName="pokemon.name" :pokeId="pokemon.id" />
         <cImage :imgUrl="pokemon.img" />
       </div>
+          <BasePopover
+      v-if="isPopoverVisible"
+      :popover-options="popoverOptions"
+      @closePopover="closePopover"
+    >
+        <BasePopoverContent :pokemon="popoverPokemon" @closePopover="isPopoverVisible = false" />
+    </BasePopover>
     </div>
   </div>
+
 </template>
 
 <script>
+import BasePopover from "./components/BasePopover";
+import BasePopoverContent from "@/components/BasePopoverContent";
 import cImage from "./components/CardImage";
 import cName from "./components/CardName";
 import Header from "./components/Header";
 import axios from "axios";
 
+var pokemonAPI = 'https://pokeapi.co/api/v2/pokemon/';
+var pokemonSpeciesAPI = 'https://pokeapi.co/api/v2/pokemon-species/';
+
 export default {
-  name: "app",
-  data() {
-    return {
-      pokemons: []
-    };
-  },
+  name: "App",
   components: {
+    BasePopover,
+    BasePopoverContent,
     cImage,
     cName,
     Header
   },
+
+
+
+  data() {
+    return {
+      isPopoverVisible: false,
+      popoverOptions: {
+        popoverReference: null,
+      
+        offset: "0,0"
+      },
+      pokemons: [],
+      popoverPokemon: {
+        name: "",
+        flavor_text: "",
+        img: "",
+      }
+    };
+  },
   async created() {
     //add async and awaits
-    for (var i = 1; i <= 4; ++i) {
-      await axios.get("https://pokeapi.co/api/v2/pokemon/" + i).then(response => {
-        this.pokemons.push({
-          id: response.data.id,
-          name: response.data.name.toUpperCase(),
-          img: response.data.sprites.front_default
+    for (var i = 1; i <= 151; ++i) {
+      await axios
+        .get(pokemonAPI + i)
+        .then(response => {
+          this.pokemons.push({
+            id: response.data.id,
+            name: response.data.name.toUpperCase(),
+            img: response.data.sprites.front_default
+          });
+        })
+        .catch(e => {
+          console.log(e);
         });
-      })
-      .catch(e => {
-        console.log(e);
-      });
     }
   },
+  mounted() {
+    this.popoverOptions.popoverReference = this.$refs.popoverReference;
+  },
+
   methods: {
+    closePopover() {
+      this.isPopoverVisible = false;
+      console.log("close");
+    },
+
+    async openPopover(pokemon, id) {
+      this.isPopoverVisible = true;
+      await axios
+        .get(pokemonSpeciesAPI + id)
+        .then(response => {
+          this.popoverPokemon = {
+            name : pokemon.name,
+            flavor_text : response.data.flavor_text_entries[1].flavor_text,
+            img : pokemon.img
+          }
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
     showDetails(e) {
       console.log(e);
     }
   }
 };
 </script>
+
+
 
 <style lang="scss">
 body {
@@ -74,10 +132,9 @@ body {
   margin: 8px;
 }
 
-
 #card {
   // flex: 1 0 31%; /* explanation below */
-  margin: 5px;
-  width: calc(100% * (1/3) - 9px - 1px);
+  margin: 40px;
+  width: calc(100% * (1 / 3) - 80px - 1px);
 }
 </style>
---
+++ src/components/BasePopoverContent.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    class="vue-tour-popover-content"
+    ref="PopoverContent"
+    tabindex="-1"
+    @keydown.esc="closePopover"
+  >
+    <div class="vue-tour-popover-content__header">
+      <p>{{ pokemon.name }}</p>
+      <img :src='pokemon.img'>
+    </div >
+      <p>{{ pokemon.flavor_text }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      descriptions: "Vue is Awesome!",
+      step: 0
+    };
+  },
+  props: [
+    'pokemon'
+  ],
+  computed: {
+    currentDescription() {
+      return this.descriptions;
+    },
+
+
+  },
+
+  methods: {
+    closePopover() {
+      this.$emit("closePopover");
+    }
+  },
+
+  mounted() {
+    this.$refs.PopoverContent.focus();
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.vue-tour-popover-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  background: white;
+  padding:15px;
+
+  &__header {
+    width: 100%;
+    border-bottom: solid 1px #000;
+    margin-bottom: 20px;
+  }
+
+  &__actions {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    margin: 40px 0 20px;
+  }
+
+  &__button {
+    background-color: #41B883;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 4px;
+
+    &.hidden {
+      visibility: hidden;
+    }
+  }
+}
+</style>
